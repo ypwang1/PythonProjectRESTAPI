5cbb86d4ad89b516b075532870d7a7f3e7f53eb1
Yupeng Wang <yupengori@gmail.com>
2025-07-29 14:33:09 +0200

    all get requests

--- day-66-starting-files-cafe-api/main.py
+++ day-66-starting-files-cafe-api/main.py
@@ -81,7 +81,31 @@ def all_cafes():
         })
     return jsonify(cafes=cafe_list)
 
-@app.route()
+
+@app.route("/search")
+def get_cafe_at_location():
+    query_location = request.args.get("loc")
+    result = db.session.execute(db.select(Cafe).where(Cafe.location==query_location))
+    all_cafes = result.scalars().all()
+    if all_cafes:
+        cafe_list_location = []
+        for cafe in all_cafes:
+            cafe_list_location.append({"id": cafe.id,
+                "name": cafe.name,
+                "map_url": cafe.map_url,
+                "img_url": cafe.img_url,
+                "location": cafe.location,
+                "seats": cafe.seats,
+                "has_toilet": cafe.has_toilet,
+                "has_wifi": cafe.has_wifi,
+                "has_sockets": cafe.has_sockets,
+                "can_take_calls": cafe.can_take_calls,
+                "coffee_price": cafe.coffee_price}
+            )
+        return jsonify(cafes= cafe_list_location)
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
+
 # HTTP POST - Create Record
 
 # HTTP PUT/PATCH - Update Record
